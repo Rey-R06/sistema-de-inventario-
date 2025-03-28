09d34c5317fd865b66c5e119833dda0368c9349e
reyK06 <rlliiirollooo@gmail.com>
2025-03-27 23:03:14 -0500

    feat: Crear base de datos y funciones para gestionar productos
    - Se creó la base de datos del sistema de gestión de inventario.
    - Se implementó la función para crear la tabla productos.
    - Se agregó la función para insertar productos en la base de datos.
    
    Este commit establece la estructura inicial del sistema y permite el registro de productos en la base de datos.

---
+++ gestorDeTienda.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+# Conexión (si no existe, se crea)
+conexion = sqlite3.connect("mi_tienda.db")
+cursor = conexion.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS productos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        precio REAL CHECK(precio > 0),
+        stock INTEGER DEFAULT 0,
+        categoria TEXT
+    )
+''')
+
+def create_table():# Conexión (si no existe, se crea)
+    
+    with sqlite3.connect("mi_tienda.db") as conexion:
+        cursor = conexion.cursor()
+
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS productos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT NOT NULL,
+                precio REAL CHECK(precio > 0),
+                stock INTEGER DEFAULT 0,
+                categoria TEXT
+            )
+        ''')
+        conexion.commit()
+        print("\n✅se creo la tabla productos correctamente")
+      
+
+create_table()
+
+def insert_data():
+    nombre = input("Nombre del producto: ")
+    precio = float(input("Precio del producto: "))
+    stock = int(input("Cantidad: "))
+    categoria = input("categoria del producto: ")
+
+    # Insertar en la base de datos
+    with sqlite3.connect("mi_tienda.db") as conexion:
+            cursor = conexion.cursor()
+            cursor.execute(
+                "INSERT INTO productos (nombre, precio, stock, categoria) VALUES (?, ?, ?, ?)",
+                (nombre, precio, stock, categoria)
+            )
+            conexion.commit()
+            print("\n✅ Producto añadido correctamente")
+    
+
+insert_data()

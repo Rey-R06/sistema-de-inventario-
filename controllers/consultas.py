8cc32b69893ed084eab6a2447aaec516caed8f9e
Rey-R06 <rlliiirollooo@gmail.com>
2025-03-31 22:53:22 -0500

    feat: Mejoras en consultas y formato de salida con pandas y tabulate
    - Implementada la función mostrar_productos() en consultas.py
    - Integración de 	abulate para formatear la salida de productos en consola
    - Corrección en 	raer_datos() para manejar consultas con parámetros
    - Optimización en uscar_por_nombre() para evitar errores de argumentos

--- controllers/consultas.py
+++ controllers/consultas.py
@@ -1,6 +1,7 @@
 from db_connection import create_connection
+from tabulate import tabulate
 
-def traer_datos(intruccion):
+def traer_datos(consulta, params=()):
     """
     Funcion que trae todos los datos de la tabla productos
     :param parametros: no ultiliza.
@@ -12,29 +13,43 @@ def traer_datos(intruccion):
     with create_connection() as conexion: # Conectar a la base de datos (se crea si no existe)
         cursor = conexion.cursor() # Crear un cursor para ejecutar consultas
          # Consulta SQL para obtener todos los registros
-        cursor.execute(intruccion)# Ejecutar la consulta
+        cursor.execute(consulta, params)# Ejecutar la consulta
         datos = cursor.fetchall()# Obtener todos los resultados
         # Mostrar los datos    
         if datos:
-            for fila in datos:
-                print(fila)  
+            return datos
         else:
-            print("📭 No hay productos registrados.")
+            return "📭 No hay productos registrados."
 
 def solicitar_datos_producto():
-    print(traer_datos("SELECT * FROM productos"))
+    datos = traer_datos("SELECT * FROM productos")
+    if datos:
+        headers = ["Id", "Nombre", "Precio", "Cantidad", "Categoria"]
+        print(tabulate(datos, headers=headers, tablefmt="grid"))  # Formato bonito
+    else:
+        print("📭 No hay productos registrados.")
     while True:
         try:
-            eleccion = int(input(("Buscar por: \n1.Nombre \n2-Precio \n3-Cantidad \n4-Categoria \-0-Salir")))
+            eleccion = int(input(("Buscar por: \n1.Nombre \n2-Precio \n3-Cantidad \n4-Categoria \n-0-Salir\n-")))
 
             if eleccion == 0:
                 break
+
+            if eleccion == 1:
+                buscar_por_nombre()
+        
+    
             
         except ValueError:
             print("Ingrese un valor valido")
 
-def filtrar_por_nombre():
-    print
+def buscar_por_nombre():
+    nombre = input("Ingrese el nombre: ")
+    datos = traer_datos("SELECT * FROM productos WHERE nombre = ?", (nombre,))
+    if datos:
+        headers = ["Id", "Nombre", "Precio", "Cantidad", "Categoria"]
+        print(tabulate(datos, headers=headers, tablefmt="grid"))  # Formato bonito
+
 
 
 #nombre  
